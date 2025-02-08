8bc6d1930765a2e8425c9ab561bfc68ffff5b8ff
ssc1241 <sscjavalove@gmail.com>
2025-02-08 19:14:56 +0530

    javaclass added
    Test.java file

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+package com.sscjava.thread;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		system.out.println("Hello World!!");
+
+	}
+
+}
